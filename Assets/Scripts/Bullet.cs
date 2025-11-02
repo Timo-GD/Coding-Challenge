32d3bfd640225645060a5047aa46a55e882159db
Timo-GD <omit400@gmail.com>
2025-11-02 13:48:00 +0100

    Made the bullet shoot forward in the direction the player is looking

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 
 public class Bullet : Item
 {
+    private int _bulletSpeed = 50;
+    private bool _isHit;
     private void Awake()
     {
         Rigidbody = GetComponent<Rigidbody>();
         BoxColliders = GetComponentsInChildren<BoxCollider>();
     }
 
-    public void Fire()
+    public void Fire(Vector3 targetPosition)
     {
-        Debug.Log("Fire");
+        Vector3 targetDirection = targetPosition - transform.position;
+        Rigidbody.AddForce(targetDirection.normalized * _bulletSpeed, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        _isHit = true;
     }
 }
