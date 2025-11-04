ad62144ef35fd4c19b453bf9ef551a91ca023c30
Timo-GD <omit400@gmail.com>
2025-11-04 11:24:00 +0100

    Made items be able to be pickup up with a spherecastnonalloc

--- Assets/Scripts/PickupCast.cs
+++ Assets/Scripts/PickupCast.cs
@@ -18,24 +18,27 @@ public class PickupCast : MonoBehaviour
     private void TryPickUp()
     {
 
-        Debug.Log(Physics.SphereCastNonAlloc(new Ray(transform.position, transform.forward), 1.25f, _itemCastHits, Mathf.Infinity, _itemMask));
+        // Debug.Log("After" + _itemCastHits.Length);
+        // Debug.Log(Physics.SphereCastNonAlloc(new Ray(transform.position, transform.forward), 1.25f, _itemCastHits, Mathf.Infinity, _itemMask));
+        // Debug.Log("Before" + _itemCastHits.Length);
         // Debug.Log(Physics.SphereCast(transform.position, 5f, transform.forward, out RaycastHit ItemHit, Mathf.Infinity, _itemMask));
-        // if (Physics.SphereCastNonAlloc(transform.position, 5f, transform.forward, _itemCastHits, Mathf.Infinity, _itemMask) == 0)
-        return;
+        if (Physics.SphereCastNonAlloc(transform.position, 1.25f, transform.forward, _itemCastHits, Mathf.Infinity, _itemMask) == 0)
+            return;
 
-        for(int i = 0; i < _itemCastHits.Length; i++)
-        {
-            // if (!_itemCastHits[i].collider.TryGetComponent<Item>(out Item item))
-            //     continue;
+        // for(int i = 0; i < _itemCastHits.Length; i++)
+        // {
+        // if (!_itemCastHits[i].collider.TryGetComponent<Item>(out Item item))
+        //     continue;
 
-            // item.Equip();
+        // item.Equip();
 
-            if (!GetComponentInParent<InverntorySystem>().Equip(_itemCastHits[i].collider.gameObject))
-                continue;
+        if (!GetComponentInParent<InverntorySystem>().Equip(_itemCastHits[0].collider.gameObject))
+            return;
+                // continue;
 
-            _itemCastHits[i].collider.GetComponent<Item>().Equip();
+        _itemCastHits[0].collider.GetComponent<Item>().Equip();
             
-        }
+        // }
     }
 
     private void OnDrawGizmos()
