bd3c74b3128c29697da1d8fac2e9815c69c82ea2
Timo-GD <omit400@gmail.com>
2025-11-05 18:55:56 +0100

    Made item switching work (Ugly version with comments incase of needing to go back)

--- Assets/Scripts/NewInventorySystem.cs
+++ Assets/Scripts/NewInventorySystem.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +6,11 @@ public class NewInventorySystem : MonoBehaviour
 {
     [SerializeField] private InputAction _switchItem;
     private Dictionary<GameObject, Item> _inventoryItems = new();
+
+    private void Awake()
+    {
+        _switchItem.performed += context => SwitchItems();
+    }
 
     private void LateUpdate()
     {
@@ -22,6 +26,128 @@ public class NewInventorySystem : MonoBehaviour
         }
     }
 
+    private void SwitchItems()
+    {
+
+        if (_switchItem.ReadValue<float>() == 0)
+            return;
+
+        PickupCast[] hands = GetComponentsInChildren<PickupCast>();
+
+        if (hands.Length == 0)
+            return;
+
+        Item[] oldItems = new Item[hands.Length];
+
+        for (int i = 0; i < hands.Length; i++)
+            oldItems[i] = hands[i].OldItem;
+
+        for(int i = 0; i < hands.Length; i++)
+        {
+            int targetIndex;
+
+            if (_switchItem.ReadValue<float>() > 0)
+                targetIndex = (i + 1) % hands.Length;
+            else
+                targetIndex = (i - 1 + hands.Length) % hands.Length;
+
+            hands[targetIndex].SwitchItem(oldItems[i]);
+
+            Debug.Log("Hand Item:   " + hands[targetIndex].gameObject);
+
+            foreach (KeyValuePair<GameObject, Item> items in _inventoryItems)
+            {
+                Debug.Log("Items    " + items);
+            }
+            if (hands[targetIndex].HeldItem != null)
+                _inventoryItems[hands[targetIndex].gameObject] = hands[targetIndex].HeldItem;
+            else
+                _inventoryItems.Remove(hands[targetIndex].gameObject);
+
+            // if (_inventoryItems.ContainsKey(hands[targetIndex].gameObject) && hands[i].HeldItem != null)
+            //     _inventoryItems[hands[targetIndex].gameObject] = hands[i].HeldItem;
+            // else if (_inventoryItems.ContainsKey(hands[targetIndex].gameObject) && hands[i].HeldItem == null)
+            //     _inventoryItems.Remove(hands[targetIndex].gameObject);
+            // else if (!_inventoryItems.ContainsKey(hands[targetIndex].gameObject) && hands[i].HeldItem != null)
+            //     _inventoryItems[hands[targetIndex].gameObject] = hands[i].HeldItem;
+            // else
+            //     return;
+
+            // if (hands[targetIndex].OldItem == null)
+            //     _inventoryItems.Remove(hands[targetIndex].gameObject);
+            // else
+            //     _inventoryItems[hands[targetIndex].gameObject] = hands[i].HeldItem;
+            
+        }
+
+        // PickupCast[] hands = GetComponentsInChildren<PickupCast>();
+        // if (hands.Length == 0)
+        //     return;
+        // for (int i = 0; i < hands.Length; i++)
+        // {
+        //     Debug.Log("Curent hand: " + hands[i]);
+        //     if (_switchItem.ReadValue<float>() > 0)
+        //         hands[i + 1 >= hands.Length ? 0 : i + 1].SwitchItem(hands[i].OldItem);
+        //     else if (_switchItem.ReadValue<float>() < 0)
+        //         hands[i - 1 < 0 ? hands.Length - 1 : i - 1].SwitchItem(hands[i].OldItem);
+
+
+        //     /*(Debug.Log("Inventory:   " + hands[i].gameObject + "   " + hands[i].HeldItem);
+        //     if (hands[i].HeldItem == hands[i + 1 >= hands.Length ? 0 : i + 1].HeldItem)
+        //     {
+        //         hands[i].SwitchItem(null);
+        //         continue;
+        //     }
+
+        //     if (_switchItem.ReadValue<float>() > 0)
+        //         hands[i].SwitchItem(hands[i + 1 >= hands.Length ? 0 : i + 1].HeldItem);
+        //     else if (_switchItem.ReadValue<float>() < 0)
+        //         hands[i].SwitchItem(hands[i - 1 < 0 ? hands.Length - 1 : i - 1].HeldItem);
+
+
+        //     if (hands[i].HeldItem == null)
+        //         _inventoryItems.Remove(hands[i].gameObject);
+        //     else
+        //         _inventoryItems[hands[i].gameObject] = hands[i].HeldItem;
+        //         */
+        // }
+        // for (int i = 0; i < _inventoryItems.Count; i++)
+        // {
+        //     // Debug.Log("Inventory count:  " + _inventoryItems.Count);
+        //     // Debug.Log("I:   " + i);
+        //     // Debug.Log(_inventoryItems.ElementAt(i));
+        //     // if (_switchItem.ReadValue<float>() > 0)
+        //     //     Debug.Log(_inventoryItems[_inventoryItems.ElementAt(i + 1 == _inventoryItems.Count ? i : i + 1).Key]);
+        //     // else if(_switchItem.ReadValue<float>() < 0)
+        //     //     Debug.Log(_inventoryItems[_inventoryItems.ElementAt(i - 1).Key]);
+
+        //     // Debug.Log("I:   " + i + "Dictionary:    " + _inventoryItems.ElementAt(i));
+
+        //     if (_switchItem.ReadValue<float>() > 0)
+        //         _inventoryItems[_inventoryItems.ElementAt(i).Key] = (i + 1 >= _inventoryItems.Count) ? _inventoryItems.ElementAt(0).Value : _inventoryItems.ElementAt(i + 1).Value;
+        //     else if (_switchItem.ReadValue<float>() < 0)
+        //         _inventoryItems[_inventoryItems.ElementAt(i).Key] = (i - 1 < 0) ? _inventoryItems.ElementAt(_inventoryItems.Count).Value : _inventoryItems.ElementAt(i - 1).Value;
+        //     else
+        //         continue;
+
+        //     // if(i + 1 >= _inventoryItems.Count)
+        //     // {
+        //     //     _inventoryItems.Add()
+        //     // }
+
+        //     Debug.Log(_inventoryItems.ElementAt(i));
+
+        //     if (_inventoryItems.ElementAt(i).Value == null)
+        //         _inventoryItems.Remove(_inventoryItems.ElementAt(i).Key);
+
+        //     // _inventoryItems[_inventoryItems.ElementAt(i).Key] = (i + 1 == _inventoryItems.Count) ? _inventoryItems.Remove(_inventoryItems.ElementAt(i).Key) : _inventoryItems.ElementAt(i + 1).Value;//_inventoryItems.ElementAt(i + 1 == _inventoryItems.Count ? i : i + 1).Value == null ? null : _inventoryItems.ElementAt(i == _inventoryItems.Count ? 0 : i + 1).Value;
+
+        //     // _inventoryItems[_inventoryItems.ElementAt(i).Key].GetComponent<PickupCast>().SwitchItem(_inventoryItems.ElementAt(i).Value);
+
+        // }
+        
+
+    }
 
     public bool Equip(GameObject hand, Item equipable)
     {
@@ -45,5 +171,18 @@ public class NewInventorySystem : MonoBehaviour
         _inventoryItems.Remove(hand);
     }
 
+    private void OnDestroy()
+    {
+        _switchItem.performed -= context => SwitchItems();
+    }
 
+    private void OnEnable()
+    {
+        _switchItem.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _switchItem.Disable();
+    }
 }
