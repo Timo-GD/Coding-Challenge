7d93561c317bd84e20677769891084b93e2549e1
Timo-GD <omit400@gmail.com>
2025-11-05 10:43:27 +0100

    Added a pickup function

--- Assets/Scripts/PickupCast.cs
+++ Assets/Scripts/PickupCast.cs
@@ -1,65 +1,63 @@
+using System;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PickupCast : MonoBehaviour
 {
     [SerializeField] private InputAction _pickup;
+    [SerializeField] private InputAction _drop;
 
-    RaycastHit[] _itemCastHits = new RaycastHit[1];
-    LayerMask _itemMask;
-    LayerMask _armorMask;
+    private Item _heldItem;
+    private NewInventorySystem _inventorySystem;
+    private RaycastHit[] _itemCastHits = new RaycastHit[1];
+    private LayerMask _itemMask;
+    private LayerMask _armorMask;
 
     private void Awake()
     {
         _pickup.performed += context => TryPickUp();
+
+        
+
         _itemMask = LayerMask.GetMask("Item");
+        _inventorySystem = GetComponentInParent<NewInventorySystem>();
     }
+
 
     private void TryPickUp()
     {
 
-        // Debug.Log("After" + _itemCastHits.Length);
-        // Debug.Log(Physics.SphereCastNonAlloc(new Ray(transform.position, transform.forward), 1.25f, _itemCastHits, Mathf.Infinity, _itemMask));
-        // Debug.Log("Before" + _itemCastHits.Length);
-        // Debug.Log(Physics.SphereCast(transform.position, 5f, transform.forward, out RaycastHit ItemHit, Mathf.Infinity, _itemMask));
-        if (Physics.SphereCastNonAlloc(transform.position, 1.25f, transform.forward, _itemCastHits, Mathf.Infinity, _itemMask) == 0)
+        if (_heldItem != null)
             return;
-
-        // for(int i = 0; i < _itemCastHits.Length; i++)
-        // {
-        // if (!_itemCastHits[i].collider.TryGetComponent<Item>(out Item item))
-        //     continue;
-
-        // item.Equip();
-
-        if (!GetComponentInParent<InverntorySystem>().Equip(_itemCastHits[0].collider.gameObject))
-            return;
-                // continue;
-
-        _itemCastHits[0].collider.GetComponent<Item>().Equip();
             
-        // }
+        if (Physics.SphereCastNonAlloc(transform.position, .6f, transform.forward, _itemCastHits, 0, _itemMask) == 0)
+            return;
+
+        if (!_inventorySystem.Equip(gameObject, _itemCastHits[0].collider.GetComponent<Item>()))
+            return;
+
+        _heldItem = _itemCastHits[0].collider.GetComponent<Item>();
+        _itemCastHits[0].collider.GetComponent<Item>().Equip(gameObject);
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(transform.position, 1.25f);
-
-    }
 
     private void OnDestroy()
     {
         _pickup.performed -= context => TryPickUp();
+
     }
 
     private void OnEnable()
     {
         _pickup.Enable();
+        _drop.Disable();
     }
 
     private void OnDisable()
     {
         _pickup.Disable();
+        _drop.Disable();
     }
 }
