9dc075e1915d8913ececb9672c350a544b8f5f20
Timo-GD <omit400@gmail.com>
2025-11-07 23:22:20 +0100

    made the door rotate

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -6,18 +6,23 @@ namespace CodingChallenge.Interactable
     public class Door : InteractableObject
     {
         private bool _isClosed;
+        private bool _isUsed;
         private float _openSpeed = 5f;
         public override bool Use()
         {
-            Debug.Log("Chek");
+            if (_isUsed)
+                return false;
+
+            _isUsed = true;
+
             if (!_isClosed)
             {
-                Quaternion targetRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y * -90, transform.rotation.z);
+                Quaternion targetRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y - 90, transform.rotation.z);
                 StartCoroutine(OpenDoor(targetRotation));
             }
             else
             {
-                Quaternion targetRotation = Quaternion.Euler(transform.rotation.x, Mathf.Abs(transform.rotation.y * 90), transform.rotation.z);
+                Quaternion targetRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
                 StartCoroutine(CloseDoor(targetRotation));
             }
 
@@ -26,23 +31,25 @@ namespace CodingChallenge.Interactable
 
         private IEnumerator OpenDoor(Quaternion targetRotation)
         {
-            while (Vector3.Distance(transform.rotation.eulerAngles, targetRotation.eulerAngles) > 0.5)
+            while (Vector3.Distance(transform.rotation.eulerAngles, targetRotation.eulerAngles) > 0.0005f)
             {
                 transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * _openSpeed);
                 yield return null;
             }
             _isClosed = true;
+            _isUsed = false;
             yield return null;
         }
         
         private IEnumerator CloseDoor(Quaternion targetRotation)
         {
-            while (Vector3.Distance(transform.rotation.eulerAngles, targetRotation.eulerAngles) > 0.5)
+            while (Vector3.Distance(transform.rotation.eulerAngles, targetRotation.eulerAngles) > 0.0005f)
             {
                 transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * _openSpeed);
                 yield return null;
             }
             _isClosed = false;
+            _isUsed = false;
             yield return null;
         }
     }
