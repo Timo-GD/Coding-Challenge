2f88908e763d8a1db1cfc65239add87ca2fdf1bf
Timo-GD <omit400@gmail.com>
2025-11-07 16:15:13 +0100

    Made a very ugly version of the button minigame

--- Assets/Scripts/Minigames/ButtonGameManager.cs
+++ Assets/Scripts/Minigames/ButtonGameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ButtonGameManager : MonoBehaviour
@@ -9,12 +10,18 @@ public class ButtonGameManager : MonoBehaviour
     [SerializeField] private Button _blueButton;
     [SerializeField] private Button _yellowButton;
 
-    private Renderer[] _answerColors;
+    private Renderer[] _pressedColors;
+    private Color[] _answerColors = { Color.gray, Color.gray, Color.gray, Color.gray };
     private int _index = 0;
 
     private void Awake()
     {
-        _answerColors = GetComponentsInChildren<Renderer>();
+        _pressedColors = GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < _pressedColors.Length; i++)
+        {
+            _answerColors[i] = _pressedColors[i].material.color;
+            _pressedColors[i].material.color = Color.gray;
+        }
 
         _redButton.OnPress += (buttonColor) => UpdateCode(buttonColor);
         _greenButton.OnPress += (buttonColor) => UpdateCode(buttonColor);
@@ -30,15 +37,37 @@ public class ButtonGameManager : MonoBehaviour
             return;
         }
 
-        _answerColors[_index].material.color = buttonColor;
+        _pressedColors[_index].material.color = buttonColor;
         _index++;
+        if(_index > 3)
+            CheckPassword();
     }
-    
+
     private void ResetCode()
     {
         _index = 0;
+        for (int i = 0; i < _pressedColors.Length; i++)
+            _pressedColors[i].material.color = Color.gray;
+    }
+    
+    private void CheckPassword()
+    {
+        for (int i = 0; i < _pressedColors.Length; i++)
+            Debug.Log("Pressed Color:   " + _pressedColors[i].material.color);
         for (int i = 0; i < _answerColors.Length; i++)
-            _answerColors[i].material.color = Color.gray;
+            Debug.Log("Anser Color: " + _answerColors[i]);
+        int isCorrect = 0;
+        if (_pressedColors[0].material.color == _answerColors[0])
+            isCorrect++;
+        if (_pressedColors[1].material.color == _answerColors[1])
+            isCorrect++;
+        if (_pressedColors[2].material.color == _answerColors[2])
+            isCorrect++;
+        if (_pressedColors[3].material.color == _answerColors[3])
+            isCorrect++;
+        Debug.Log(isCorrect);
+        if (isCorrect == 4)
+            Debug.Log("B-B-B-B-B-Bingo");
     }
 
 }
