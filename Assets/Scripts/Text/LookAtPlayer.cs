978f6093353f1554fded0478332a85640e9b1ef7
Timo-GD <omit400@gmail.com>
2025-11-08 23:27:08 +0100

    Made the text from the flashlight look at the player

---
+++ Assets/Scripts/Text/LookAtPlayer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+    private void Update()
+    {
+        transform.LookAt(new Vector3(_playerTransform.position.x, _playerTransform.position.y, _playerTransform.position.z));
+    }
+}
