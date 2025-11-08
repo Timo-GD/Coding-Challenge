27682644f6b4c37d7ed9b3599cf83613f1778e00
Timo-GD <omit400@gmail.com>
2025-11-09 00:47:43 +0100

    More code cleanup

--- Assets/Scripts/Minigames/MinigameManager.cs
+++ Assets/Scripts/Minigames/MinigameManager.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class MinigameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _hatPrefab;
-    
-    private static MinigameManager _instance;
     public static MinigameManager Instance => _instance;
+    
+    [SerializeField] private GameObject _hatPrefab;
+
+    private static MinigameManager _instance;
     private int _count;
 
     private void Awake()
