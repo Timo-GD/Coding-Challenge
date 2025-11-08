409e0851d8cf45969e1bc541031100e51ddab19b
Timo-GD <omit400@gmail.com>
2025-11-08 19:38:30 +0100

    Added an ugly minigame manager

---
+++ Assets/Scripts/Minigames/MinigameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MinigameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _hatPrefab;
+    private static MinigameManager _instance;
+    public static MinigameManager Instance => _instance;
+    private int _count;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    
+    public void UpdateMinigameCount()
+    {
+        _count++;
+
+        if (_count == 2)
+            WinPrize();
+    }
+
+    private void WinPrize()
+    {
+        GameObject prize = Instantiate(_hatPrefab, transform);
+    }
+}
