c782a9697591e1a6e86a0321d4c33761c63f34c6
Timo-GD <omit400@gmail.com>
2025-11-08 19:59:45 +0100

    Made the bullets move slower to have more consistent collisions

--- Assets/Scripts/Items/Bullet.cs
+++ Assets/Scripts/Items/Bullet.cs
@@ -4,16 +4,21 @@ namespace CodingChallenge.Items
 {
     public class Bullet : Item
     {
-        private int _bulletSpeed = 50;
+        private int _bulletSpeed = 15;
 
         public void Fire(Vector3 targetPosition)
         {
             _rigidbody.linearVelocity = Vector3.zero;
             _rigidbody.angularVelocity = Vector3.zero;
+            _rigidbody.useGravity = false;
 
             Vector3 targetDirection = targetPosition - transform.position;
             transform.rotation = Quaternion.Euler(targetDirection);
             _rigidbody.AddForce(targetDirection.normalized * _bulletSpeed, ForceMode.Impulse);
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            _rigidbody.useGravity = true;
         }
     }
 }
